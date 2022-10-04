779ebfab79e00cc2440a3412689037e8c921a8d9
Danny Huang <danny.huang@usask.ca>
2022-10-04 11:32:50 -0600

    updated ripple

--- src/artisynth/demos/growth/models/paper/Intricate_RippleCascade.java
+++ src/artisynth/demos/growth/models/paper/Intricate_RippleCascade.java
@@ -8,24 +8,10 @@ import artisynth.demos.growth.models.base.GrowDemo;
 import maspack.geometry.Vertex3d;
 import maspack.matrix.Point3d;
 
-/**
+/* -model artisynth.demos.growth.models.paper.Intricate_RippleCascade
+ * 
  * Grow up to 20 seconds in 6.66 intervals.
- * 
- * Camera: Front view: 0.00221373 -3.18407 0.00573919 0.00221373 -0.0263626
- * 0.00573919
- * 
- * Front view (alt for video):
- * 
- * 
- * mCameraEye = new Point3d(0, -3.18586, 0); mCameraCenter = new Point3d(0, 0,
- * 0);
- * 
- * Perspective: 1.981 -1.84654 0.301408 0.302009 -0.301062 -0.0870515
- * 
- * Close-up: mCameraEye = new Point3d(0.119382, -0.985104, 0.098211);
- * mCameraCenter = new Point3d(-0.138341, -0.747875, 0.038583);
  */
-// -model artisynth.demos.growth.models.paper.Intricate_RippleCascade
 public class Intricate_RippleCascade extends GrowDemo {
 
    protected double morphogenSrcDuration = 999;
@@ -35,57 +21,84 @@ public class Intricate_RippleCascade extends GrowDemo {
 
       mMeshX = 2;
       mMeshY = 1;
-
-      mSizeMin = 0.01;
-      mSizeMax = mSizeMin * 100;
       mMeshXDiv = 16;
       mMeshYDiv = 8;
+
+      mRemeshFreq = 0.25;
+      mSizeMin = 0.01;
+      mSizeMax = mSizeMin * 100;
+
       m_shellThickness = 0.001;
-      m_youngsModulus = 1e6;
+      m_youngsModulus = 1e4;
 
       mDiffusionTimestepScale = 0.00001;
-
-      mIsActivatePAR = true;
+      mIsActivatePAR = false;
       mIsActivatePER = true;
 
-      // Benchmark:
-      mRenderMode = RenderMode.MORPHOLOGY;
-      mPauseEveryInterval = 20;
-      mEnableCollisionHandling = false;
-      mRemeshFreq = 0.25;
-
-      // Post bug fix: Young modulus was previous capped to 1e5
-      m_youngsModulus = 1e5;
-
       mShowColorBar = false;
-      mCameraEye = new Point3d (0, -3.18586, 0);
-      mCameraCenter = new Point3d (0, 0, 0);
 
       // Collision test
-      mPauseEveryInterval = 20;
       mEnableCollisionHandling = false;
       mEnableProximityDetection = true;
       mEnableContinuousDetection = false;
       mEnableImpactZoneDetection = true;
       mPenetrationTol = -1e-3;
+
+      mPauseEveryInterval = 18;
+
+      // Plastic Embed figure - All
+      // mPauseEveryInterval = 13.5;
+      // mEnablePlasticEmbedding = true;
+      // mEnableRemesh = true;
+
+      // Plastic Embed figure - Remesh
+      // mPauseEveryInterval = 13.5;
+      // mEnablePlasticEmbedding = false;
+      // mEnableRemesh = true;
+
+      // Plastic Embed figure - Plastic
+      // mPauseEveryInterval = 13.5;
+      // mEnablePlasticEmbedding = true;
+      // mEnableRemesh = false;
+
+      // Plastic Embed figure - None
+      // mPauseEveryInterval = 7.00;
+      // mEnablePlasticEmbedding = false;
+      // mEnableRemesh = false;
    }
 
    protected void build_renderConfig () {
       super.build_renderConfig ();
 
-      RenderConfig cfg = mRendCfgPresets.get (RenderMode.DEFAULT);
-      cfg.mNodeRadius = 0.01;
-
-      cfg = mRendCfgPresets.get (RenderMode.MORPHOLOGY);
+      RenderConfig cfg = mRendCfgPresets.get (RenderMode.MORPHOLOGY);
       cfg.mFrontMeshColor = Color.RED;
-      cfg.mBackgroundColor = Color.white;
+      cfg.mBackgroundColor = Color.WHITE;
 
-      // cfg = mRendCfgPresets.get (RenderMode.TOPOLOGY);
-      // cfg.mFrontMeshColor = new Color(250,250,250);
+      // Front-View
+      // mCameraEye = new Point3d (0.00221373, -3.18407, 0.00573919);
+      // mCameraCenter = new Point3d (0.00221373, -0.0263626, 0.00573919);
 
-      // mRendCfg = mRendCfgPresets.get (RenderMode.TOPOLOGY);
-      // mRendCfg.mFrontMeshColor = new Color(242,242, 242);
-      // mRendCfg.mDrawEdges = true;
+      // Perspective
+      // mCameraEye = new Point3d (1.981, -1.84654, 0.301408);
+      // mCameraCenter = new Point3d (0.302009, -0.301062, -0.0870515);
+
+      // Close-up
+      mCameraEye = new Point3d (0.214431, -0.687118, 0.08121);
+      mCameraCenter = new Point3d (-0.0160858, -0.521149, 0.0225617);
+
+      // Top-Down for ablation topology
+      // mCameraEye = new Point3d (-0.0103429, -0.00136385, 2.64955);
+      // mCameraCenter = new Point3d (-0.0103429, -0.00136385, -0.00338145);
+      cfg = mRendCfgPresets.get (RenderMode.TOPOLOGY);
+      // cfg.mFrontMeshColor = new Color (204, 204, 204);
+      // cfg.mRearMeshColor = new Color (204, 204, 240);
+      // cfg.mBackgroundColor = Color.WHITE;
+      // cfg.mMeshEdgeColor = Color.BLACK;
+
+      // Ablation rest figure color
+      // cfg.mFrontMeshColor = cfg.mRearMeshColor;
+
+      mRendCfg = cfg;
    }
 
    protected void build_post () {
@@ -105,7 +118,7 @@ public class Intricate_RippleCascade extends GrowDemo {
       for (int v = 0; v < mMesh[0].numVertices (); v++) {
          GrowNode3d gNode = (GrowNode3d)mFemModel[0].getNode (v);
          if (gNode.mIsMorphogenSrc) {
-            gNode.mChems.set (3, 1);
+            gNode.mChems.set (3, 1.0);
          }
       }
 
